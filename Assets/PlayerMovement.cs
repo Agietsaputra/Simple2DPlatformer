23e1df654f00a3a4cbf3c37008f8e4a8c0b521e6
topikk <118111616+taufiqpr@users.noreply.github.com>
2025-05-21 11:23:39 +0700

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float horizontalInput;
+    float moveSpeed = 5f;
+    bool isFacingRight = true; // default harus true
+    float jumpPower = 5f;
+    bool isGrounded = false;
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        FlipSprite();
+
+        if (Input.GetButtonDown("Jump") && isGrounded) // ganti !isGrounded jadi isGrounded
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isGrounded = false;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x)); // gunakan Mathf, bukan Math
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls; // ❌ kamu tulis "1s" yang typo
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) // ❌ harusnya OnCollisionEnter2D, bukan OnTriggerEnter2D
+    {
+        isGrounded = true;
+        animator.SetBool("IsJumping", false);
+    }
+}
